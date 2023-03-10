20c9d57fa80598f007b46ded5e6c22ba2120ded7
RedSorm <juraszwojtek03@gmail.com>
2023-03-10 14:28:40 +0100

    test

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-x = int(input("Podaj prędkość: "))
-if x <60:
-    print("bez mandatu")
-elif 61< x <= 80:
-    print("niski mandat")
-else:
-    print("Wysoki mandat")
+n = int(input("podaj przypadki"))
+while n>0:
+    x = int(input("Podaj prędkość: "))
+    y = input("Czy masz urodziny? Wpisz tak lub nie:  ")
+    if y == "tak":
+        if x <= 65:
+            print("bez mandatu")
+        elif 66<=  x <= 85:
+                print("niski mandat")
+        else:
+                print("Wysoki mandat")
+    else:
+        if x <= 60:
+            print("bez mandatu")
+        elif 61<=  x <= 80:
+                print("niski mandat")
+        else:
+                print("Wysoki mandat")
+    n -=1
